a840406e81506da8c1d11c00354dda05a83cb4fe
Bipeen swaraj <120006987+bipeenswaraj@users.noreply.github.com>
2025-01-17 20:14:46 +0530

    Add files via upload

---
+++ 1st.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int broNum;
+    cout<<"Enter number of brother: ";
+    cin>>broNum;
+    
+    if(broNum==0){
+        cout<<"Baat bn gayi";
+    }else{
+        cout<<"lee baat nhi bni..ab??";
+    }
+    
+    return 0;
+}
