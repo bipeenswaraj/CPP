ca73943bb0f4e958b3a4124f84554c941f2f9678
Bipeen swaraj <120006987+bipeenswaraj@users.noreply.github.com>
2025-02-16 18:50:19 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=5;
+    cout<<(++a)*(++a);
+    //in right shift case we divide the number from 2 
+    //and in left cast multiply by 2.
+    // int a=-13;
+    // a=a>>1;
+    // cout<<a;
+    // bool a = true;
+    // bool b=false;
+   // cout<<(a^b);
+    // cout<<(2&3)<<endl;
+    // cout<<(5&10);
+    // cout<<(3|7);
+    // cout<<~(~a);
+    return 0;
+}
