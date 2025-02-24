b9301568d5ee78298e63b92589c6b0397f93fd17
Bipeen swaraj <120006987+bipeenswaraj@users.noreply.github.com>
2025-02-24 22:45:20 +0530

    Add files via upload

---
+++ break and continue.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    for(int i=0;i<5;i++){
+        continue;
+        cout<<"Love bubber"<<endl;
+    }
+    // for(int i=0;i<5;i++){
+    //     cout<<"Babbar"<<endl;
+    //     break;
+    // }
+    // cout<<"Love"<<endl;
+    return 0;
+}
