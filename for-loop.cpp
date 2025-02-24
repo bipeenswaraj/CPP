cfa10d08afa1828a4d2d1acefd0d36546f868897
Bipeen swaraj <120006987+bipeenswaraj@users.noreply.github.com>
2025-02-24 22:46:50 +0530

    Add files via upload

---
+++ for-loop.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // for(int i=0; i<5; i++){
+    //     cout<<"Hello India!"<<endl;
+    // }
+    
+    // for(int i=7; i>0;i--){
+    //     cout<< "i = "<<i <<endl;
+    // }
+    
+    // for(int i=1 ;i<=10; i++){
+    //     cout<< 2*i <<endl;
+    // }
+    
+    // for(int i=0; i<=5; i=i+2){
+    //     cout<<i<<endl;
+    // }
+    
+    // for(int i=1; i<10; i=i*2){
+    //     cout<<i<<endl;
+    // }
+    
+    // for(int i=100; i>0; i=i/2){
+    //     cout<<i<<endl;
+    // }
+    
+    // for(int i=5; (i>=0 && i<10); i++){
+    //     cout<<i<<endl;
+    //     }
+    
+    // int i=0;
+    // for( ; ; ){
+    //     if(i<5){
+    //         i++;
+    //         cout<<i<<endl;
+    //     }
+    // }
+    
+    // int n;
+    // if(cin>>n){
+    //     cout<<"Hello"<<endl;
+    // }
+    
+    // if(cout<<"Love"){
+    //     cout<<"luv";
+    // }
+    return 0;
+}
